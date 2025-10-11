5b2dcce16df6bae4e76d45cc16bdb96f6b7fe6e9
MedNewton <med.newton.2013@gmail.com>
2025-10-11 07:29:35 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,6 @@ export default function Home() {
   return (
     <Stack width={"100%"} alignItems={"center"}>
       <Hero />
-      <Stack height={"100vh"} width={"100%"} overflow={"hidden"}>
-        
-      </Stack>
     </Stack>
   );
 }
