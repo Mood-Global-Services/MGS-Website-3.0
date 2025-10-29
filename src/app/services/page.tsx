4ac4cbb02d3522d96bb41b952b7914003eba0568
MedNewton <med.newton.2013@gmail.com>
2025-10-30 00:11:56 +0100

    .

---
+++ src/app/services/page.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "@mui/material";
+import Image from "next/image";
+import ServicesHero from "@/sections/services/servicesHero";
+import Footer from "@/components/layout/footer";
+import theme from "@/theme/theme";
+
+export default function Home() {
+
+  return (
+    <Stack width={"100%"} alignItems={"center"} gap={2}>
+      <ServicesHero />
+      <Stack width={"100%"} alignItems={"center"} gap={2} paddingBottom={4} position={"relative"}>
+       
+      </Stack>
+      
+      <Stack width={"100%"} alignItems={"center"} position={"relative"} sx={{
+        borderTop: `1px solid ${theme.palette.brand.border1.main}`,
+        marginTop: -2,
+      }}>
+        <Footer />
+      </Stack>
+
+    </Stack>
+  );
+}
---
+++ src/sections/services/servicesHero.tsx
@@ -0,0 +1,50 @@
+import theme from "@/theme/theme";
+import { Stack, Typography } from "@mui/material";
+import Image from "next/image";
+
+import servicesHeroAsset from "@/assets/images/servicesPage/servicesHeroAsset.svg?url";
+
+const ServicesHero = () => {
+    return (
+        <Stack alignItems="center"
+            justifyContent="center"
+            width="100%"
+            minHeight="40vh"
+            paddingX={4}
+            position="relative"
+            overflow="hidden"
+        >
+            <Image src={servicesHeroAsset} alt="services hero asset" width={350} height={350} style={{
+                position: "absolute",
+                top: "-20%",
+                right: "0%",
+                zIndex: 0,
+                opacity: 0.78,
+            }} />
+            <Stack width="50%" alignItems="center" justifyContent="center" gap={4} marginX="auto">
+                <Typography variant="h1" component="h1" fontSize="3.75rem !important" className="chno" fontWeight={500} textAlign="center" textTransform="uppercase" color={theme.palette.brand.mgsBlue1.main}>
+                    Services we offer
+                </Typography>
+                <Typography
+                    variant="h6"
+                    component="h6"
+                    fontWeight={300}
+                    textAlign="center"
+                    color={theme.palette.text.secondary}
+                >
+                    Our services cover the full spectrum of modern tech: blockchain infrastructure, advanced AI systems, product development, and market-ready GTM strategies with solid legal support.
+                </Typography>
+            </Stack>
+            <Image src={servicesHeroAsset} alt="services hero asset" width={350} height={350} style={{
+                position: "absolute",
+                top: "-20%",
+                left: "0%",
+                transform: "scaleX(-1)",
+                opacity: 0.78,
+                zIndex: 0,
+            }} />
+        </Stack>
+    );
+};
+
+export default ServicesHero;
