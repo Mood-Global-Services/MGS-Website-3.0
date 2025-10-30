8808dd389ac54aced720681c1f18c31427eff20f
MedNewton <med.newton.2013@gmail.com>
2025-10-30 20:18:52 +0100

    .

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -7,45 +7,72 @@ import ProductDev from "@/sections/services/productDev";
 import GTMAndLegal from "@/sections/services/gtmAndLegal";
 import Footer from "@/components/layout/footer";
 import theme from "@/theme/theme";
+import Image from "next/image";
+
+import servicesPageGradient from "@/assets/images/servicesPage/servicesPageGradients.svg?url";
 
 export default function ServicesPage() {
 
     return (
         <Stack width={"100%"} alignItems={"center"} gap={2}>
             <ServicesHero />
-            <SmartContracts />
-            <Divider id="infra-and-protocols" sx={{
-                borderBottom: "1px solid rgba(255, 255, 255, 0.17)",
-                marginY: 10,
-                marginX: "auto",
-                width: "80%",
+            <Stack width={"100%"} alignItems={"center"} position={"relative"} paddingBottom={6}>
+                <Stack width={"100%"} height={"100%"} sx={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    zIndex: 1,
+                }}>
+                    <Image src={servicesPageGradient} alt="services page gradient" width={1000} height={1000} style={{
+                        width: "100%",
+                        height: "110%",
+                        objectFit: "cover",
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        zIndex: 1,
+                        
+                    }} />
+                </Stack>
+                <Stack width={"100%"} alignItems={"center"} position={"relative"} overflow={"hidden"} sx={{
+                    zIndex: 2,
+                }}>
+                    <SmartContracts />
+                    <Divider id="infra-and-protocols" sx={{
+                        borderBottom: "1px solid rgba(255, 255, 255, 0.17)",
+                        marginY: 10,
+                        marginX: "auto",
+                        width: "80%",
 
-            }} />
-            <InfraAndProtocols />
-            <Divider id="ai-systems" sx={{
-                borderBottom: "1px solid rgba(255, 255, 255, 0.17)",
-                marginY: 10,
-                marginX: "auto",
-                width: "80%",
-            }} />
-            <AISystems />
-            <Divider id="product-dev" sx={{
-                borderBottom: "1px solid rgba(255, 255, 255, 0.17)",
-                marginY: 10,
-                marginX: "auto",
-                width: "80%",
-            }} />
-            <ProductDev />
-            <Divider id="gtm-and-legal" sx={{
-                borderBottom: "1px solid rgba(255, 255, 255, 0.17)",
-                marginY: 10,
-                marginX: "auto",
-                width: "80%",
-            }} />
-            <GTMAndLegal />
+                    }} />
+                    <InfraAndProtocols />
+                    <Divider id="ai-systems" sx={{
+                        borderBottom: "1px solid rgba(255, 255, 255, 0.17)",
+                        marginY: 10,
+                        marginX: "auto",
+                        width: "80%",
+                    }} />
+                    <AISystems />
+                    <Divider id="product-dev" sx={{
+                        borderBottom: "1px solid rgba(255, 255, 255, 0.17)",
+                        marginY: 10,
+                        marginX: "auto",
+                        width: "80%",
+                    }} />
+                    <ProductDev />
+                    <Divider id="gtm-and-legal" sx={{
+                        borderBottom: "1px solid rgba(255, 255, 255, 0.17)",
+                        marginY: 10,
+                        marginX: "auto",
+                        width: "80%",
+                    }} />
+                    <GTMAndLegal />
+                </Stack>
+            </Stack>
             <Stack width={"100%"} alignItems={"center"} position={"relative"} sx={{
                 borderTop: `1px solid ${theme.palette.brand.border1.main}`,
-                marginTop: 6,
+                zIndex: 2,
+                backgroundColor: "#0B0B0B",
             }}>
                 <Footer />
             </Stack>
